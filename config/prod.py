60042342a902c2a4489f4f4fc99083ac214473af
irudigio <irina.digiovanni@intive.com>
2021-12-31 11:07:38 -0300

    changes

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,9 @@
+import os
+
 DEBUG = False
 
 SECRET_KEY='Irina16011995'
 
-SQLALCHEMY_DATABASE_URI='postgresql://ukvodcmlfavnca:ec344d514330955cc010631b67169f658668b2f44ce5ce5a4e42c1bb4a2119a8@ec2-18-235-55-95.compute-1.amazonaws.com:5432/d61nrkc9jq4tai'
+SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL']
 
 SQLALCHEMY_TRACK_MODIFICATIONS=False
