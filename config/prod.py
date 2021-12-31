a2f5f457d3f1d4aa1aa2c3704bfa8abb3050804d
irudigio <irina.digiovanni@intive.com>
2021-12-31 10:49:11 -0300

    add files

---
+++ config/prod.py
@@ -0,0 +1,7 @@
+DEBUG = False
+
+SECRET_KEY='Irina16011995'
+
+SQLALCHEMY_DATABASE_URI='postgresql://ukvodcmlfavnca:ec344d514330955cc010631b67169f658668b2f44ce5ce5a4e42c1bb4a2119a8@ec2-18-235-55-95.compute-1.amazonaws.com:5432/d61nrkc9jq4tai'
+
+SQLALCHEMY_TRACK_MODIFICATIONS=False
--- run.py
+++ run.py
@@ -1,65 +1,14 @@
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-app = Flask (__name__)
-
-app.config.update (
-
-    SECRET_KEY='Irina16011995',
-    SQLALCHEMY_DATABASE_URI='postgresql://ukvodcmlfavnca:ec344d514330955cc010631b67169f658668b2f44ce5ce5a4e42c1bb4a2119a8@ec2-18-235-55-95.compute-1.amazonaws.com:5432/d61nrkc9jq4tai',
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-)
-
-db = SQLAlchemy(app)
-
-class Publication(db.Model):
-    __tablename__ = "publication"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return 'Publisher is {}'.format(self.name)
-
-
-class Book(db.Model):
-    __tablename__ = 'book'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(500), nullable=False, index=True)
-    author = db.Column(db.String(350))
-    avg_rating = db.Column(db.Float)
-    format = db.Column(db.String(50))
-    image = db.Column(db.String(100), unique=True)
-    num_pages = db.Column(db.Integer)
-    pub_date = db.Column(db.DateTime, default= datetime.utcnow())
-
-    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
-
-    def __init__(self, title, author, avg_rating, format, image, num_pages, pub_id):
-    
-        self.title = title
-        self.author = author
-        self.avg_rating = avg_rating
-        self.format = format
-        self.image = image
-        self.num_pages = num_pages
-        self.pub_id = pub_id
-
-    def __repr__(self):
-        return '{} by {}'.format(self.title, self.author)
-
+from app import create_app, db
+from app.auth.models import User
 
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
-
-
+    flask_app = create_app('prod')
+    with flask_app.app_context():
+        db.create_all()
+        if not User.query.filter_by(user_name='Harry').first():
+            User.create_user(user='Harry',email= 'harry@gmail.com', password="secret")
+    flask_app.run()
 
 
 
